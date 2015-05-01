NamelessRom/android_device_lge_hammerhead

hammerhead: Disable hispeed_freq while screen is off
When using apps that are active in background, severe
battery drain is observed, caused by small spikes in cpu
load that trigger frequent jumps to hispeed_freq. This
becomes very apparent with GPS tracking apps.

Add an extension to the PowerHAL to effectively disable
highspeed_freq when screen is off by choosing an
impossible value for go_hispeed_load.  interactive's
regular routines are sufficient to handle any kind
of tasks that are processed during screen off. We don't
need additional responsiveness there.

Change-Id: I9d7b76a0f9a78145300e4ce6ab65885d4f9eed92

BoardConfig.mk
@@ -160,6 +160,7 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 # QCOM PowerHAL
 TARGET_POWERHAL_VARIANT := qcom
+TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/lge/hammerhead/power/power_ext.c
 
 USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY:= true
 USE_DEVICE_SPECIFIC_CAMERA:= true

CyanHacker-Lollipop/device_lge_hammerhead

BoardConfig.mk
@@ -154,6 +154,7 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 # QCOM PowerHAL
 TARGET_POWERHAL_VARIANT := qcom
+TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/lge/hammerheadcaf/power/power_ext.c
 
 USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY:= true
 USE_DEVICE_SPECIFIC_CAMERA:= true